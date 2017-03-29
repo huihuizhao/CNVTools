6c46e616d6f60948c0ea83d0779f50061213b4e2
huihuizhao <zhaohuihui@outlook.com>
2017-03-29 15:51:24 +0800

    11

--- src/ImportTest11.java
+++ src/ImportTest11.java
@@ -6,12 +6,13 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ImportTest11 {
 
 	static Connection con = null;// 创建一个数据库连接
-	PreparedStatement pre = null;// 创建预编译语句对象，一般都是用这个而不用Statement
+	static PreparedStatement pre = null;// 创建预编译语句对象，一般都是用这个而不用Statement
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 
@@ -36,10 +37,12 @@ public class ImportTest11 {
 			Long startTime = System.currentTimeMillis();
 			PreparedStatement pst = (PreparedStatement) con.prepareStatement(sql.toString());
 
-//			 File root = new File("D:/FocusMap/Data/科学轮20150831主流系与西太平洋暖池变异机制调查航次第一航段/ctd");//0001
+			// File root = new
+			// File("D:/FocusMap/Data/科学轮20150831主流系与西太平洋暖池变异机制调查航次第一航段/ctd");//0001
 			File root = new File("D:/FocusMap/Data/数据2017011102/科学三号201405黄海春季调查航次/CTD/CTD-后处理/data");// 0002
-//			 File root = new File("D:/FocusMap/Data/数据2017011102/201606热液与冷泉综合调查航次-1/CTD");//0003
-
+			// File root = new
+			// File("D:/FocusMap/Data/数据2017011102/201606热液与冷泉综合调查航次-1/CTD");//0003
+			int cdtRowID = 1;
 			File[] files = root.listFiles();
 			for (File file : files) {
 
@@ -56,75 +59,112 @@ public class ImportTest11 {
 
 					}
 				}
-				 instring = bufferedreader.readLine();
-				 if (0 != instring.length()) {
-				 instring = instring.replaceAll("\\s+", " ");
-				 instring = instring.trim();
-				 strArr = instring.split(" ");// 与txt文件中的分隔符要一致
-				
-//				 //0001
-//				 pst.setString(1, "1");				 
-//				 pst.setString(2, file.getName().substring(0, file.getName().length() - 4));
-//				 pst.setString(1, "1");
-//				 pst.setString(3, strArr[4].trim());
-//				 pst.setString(4, strArr[13].trim());
-//				 pst.setString(5, strArr[12].trim());
-				//
-				// //0002
-				 pst.setInt(1, id);			 
-				 pst.setInt(2, file.getName().substring(0, file.getName().length() - 4));
-				 pst.setInt(3, 1);
-				 pst.setString(4, strArr[4].trim());
-				 pst.setString(5, strArr[13].trim());
-				 pst.setString(6, strArr[12].trim());
-				
-				 //0003
-//				 pst.setString(1, "0003");
-//				 pst.setString(2, file.getName().substring(0,file.getName().length() - 4));
-//				 pst.setString(3, strArr[4].trim());
-//				 pst.setString(4, strArr[12].trim());
-//				 pst.setString(5, strArr[11].trim());
-				 // 把一个SQL命令加入命令列表
-				 pst.addBatch();
-				 }
+				int staID = 0;
+				instring = bufferedreader.readLine();
+
+				if (0 != instring.length()) {
+					instring = instring.replaceAll("\\s+", " ");
+					instring = instring.trim();
+					strArr = instring.split(" ");// 与txt文件中的分隔符要一致
+
+					// //0001
+					// pst.setString(1, "1");
+					// pst.setString(2, file.getName().substring(0,
+					// file.getName().length() - 4));
+					// pst.setString(1, "1");
+					// pst.setString(3, strArr[4].trim());
+					// pst.setString(4, strArr[13].trim());
+					// pst.setString(5, strArr[12].trim());
+					//
+					// //0002
+					pst.setInt(1, cdtRowID);
+					// pst.setInt(2, file.getName().substring(0,
+					// file.getName().length() - 4));
+
+					String staName = file.getName().substring(0, file.getName().length() - 4);
+					String sqlStaID = "select * from STATION where NAME=?";
+					// int staID = 0;
+					pre = con.prepareStatement(sqlStaID);// 实例化预编译语句
+					pre.setString(1, staName);// 设置参数，前面的1表示参数的索引，而不是表中列名的索引
+					ResultSet result = null;// 创建一个结果集对象
+					result = pre.executeQuery();// 执行查询，注意括号中不需要再加参数
+					while (result.next()) {
+						staID = result.getInt("ID");
+
+					}
+
+					pst.setInt(2, staID);
+					pst.setInt(3, 1);
+					pst.setString(4, strArr[21].trim());
+					pst.setString(5, strArr[4].trim());
+					pst.setString(6, strArr[6].trim());
+
+					// 0003
+					// pst.setString(1, "0003");
+					// pst.setString(2,
+					// file.getName().substring(0,file.getName().length() - 4));
+					// pst.setString(3, strArr[4].trim());
+					// pst.setString(4, strArr[12].trim());
+					// pst.setString(5, strArr[11].trim());
+					// 把一个SQL命令加入命令列表
+					pst.addBatch();
+
+				}
 
 				while ((instring = bufferedreader.readLine()) != null) {
-
-//					0001  0002 跳跃输入
-					for (int i = 0; i < 1000; i++) {
-						instring = bufferedreader.readLine();
-					}
+					cdtRowID = cdtRowID + 1;
+					// 0001 0002 跳跃输入
+					// for (int i = 0; i < 1000; i++) {
+					// instring = bufferedreader.readLine();
+					// }
 					if (instring != null && (0 != instring.length())) {
 						instring = instring.replaceAll("\\s+", " ");
 						instring = instring.trim();
 						strArr = instring.split(" ");// 与txt文件中的分隔符要一致
-//						// 0001
-//						pst.setString(1, "0001");
-//						pst.setString(2, file.getName().substring(0, file.getName().length() - 4));
-//						pst.setString(3, strArr[4].trim());
-//						pst.setString(4, strArr[13].trim());
-//						pst.setString(5, strArr[12].trim());
+						// // 0001
+						// pst.setString(1, "0001");
+						// pst.setString(2, file.getName().substring(0,
+						// file.getName().length() - 4));
+						// pst.setString(3, strArr[4].trim());
+						// pst.setString(4, strArr[13].trim());
+						// pst.setString(5, strArr[12].trim());
 						//
-//						// 0002
-						 pst.setString(1, "1");				 
-						 pst.setString(2, file.getName().substring(0, file.getName().length() - 4));
-						 pst.setString(1, "1");
-						 pst.setString(3, strArr[4].trim());
-						 pst.setString(4, strArr[13].trim());
-						 pst.setString(5, strArr[12].trim());
+						// // 0002
+						pst.setInt(1, cdtRowID);
+						// pst.setInt(2, file.getName().substring(0,
+						// file.getName().length() - 4));
 
-						 //0003
-//						 pst.setString(1, "0003");
-//						 pst.setString(2, file.getName().substring(0,
-//						 file.getName().length() - 4));
-//						 pst.setString(3, strArr[4].trim());
-//						 pst.setString(4, strArr[12].trim());
-//						 pst.setString(5, strArr[11].trim());
-						
-						
+						String staName = file.getName().substring(0, file.getName().length() - 4);
+						String sqlStaID = "select * from STATION where NAME=?";
+						// int staID = 0;
+						pre = con.prepareStatement(sqlStaID);// 实例化预编译语句
+						pre.setString(1, staName);// 设置参数，前面的1表示参数的索引，而不是表中列名的索引
+						ResultSet result = null;// 创建一个结果集对象
+						result = pre.executeQuery();// 执行查询，注意括号中不需要再加参数
+						while (result.next()) {
+							staID = result.getInt("ID");
+
+						}
+
+						pst.setInt(2, staID);
+						pst.setInt(3, 1);
+						pst.setString(4, strArr[21].trim());
+						pst.setString(5, strArr[4].trim());
+						pst.setString(6, strArr[6].trim());
+
+						// 0003
+						// pst.setString(1, "0003");
+						// pst.setString(2, file.getName().substring(0,
+						// file.getName().length() - 4));
+						// pst.setString(3, strArr[4].trim());
+						// pst.setString(4, strArr[12].trim());
+						// pst.setString(5, strArr[11].trim());
+
 						// 把一个SQL命令加入命令列表
 						pst.addBatch();
+
 					}
+
 				}
 				f.close();
 
@@ -134,7 +174,8 @@ public class ImportTest11 {
 				con.commit();
 				Long endTime = System.currentTimeMillis();
 				System.out.println(file.getName() + "用时：" + (endTime - startTime));
-
+				
+				cdtRowID++;
 			}
 
 			pst.close();
